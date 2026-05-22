d707b8c07c26289e2bd7cb0f5d22899e4809505f
redoneabouaali-lab <redoneabouaali@gmail.com>
2026-05-22 18:59:28 +0000

    fix: remove X-Powered-By header

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
   outputFileTracingRoot: path.join(__dirname),
   images: {
     remotePatterns: [
